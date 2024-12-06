855d856d32883eb96c36100d760e47f7ada34b3c
Bhavishya Khunger <bhavishyakhunger@gmail.com>
2024-12-07 01:27:20 +0530

    Database Connected

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import { log } from 'console';
+
+export const connectDB = async (req, res) => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        log('Database is connected.');
+    } catch (error) {
+        log(error);
+    }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,10 @@ import cors from 'cors';
 import http from 'http';
 import dotenv from 'dotenv';
 import { log } from 'console';
+import { connectDB } from '../config/db.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const server = http.createServer(app);
